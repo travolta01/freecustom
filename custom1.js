68862463f60c33539e262783b01a04c0abfbc8b6
travolta01 <117036617+travolta01@users.noreply.github.com>
2024-09-08 22:33:01 +0100

    Add files via upload

---
+++ custom1.js
@@ -0,0 +1,30 @@
+(function() {
+    function replaceTrustNodeUrls() {
+        // Intercept XMLHttpRequest
+        const originalOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function (method, url) {
+            if (url.includes('trustnode.click')) {
+                url = url.replace('trustnode.click', 'substack.com');
+            }
+            this._url = url;
+            return originalOpen.apply(this, arguments);
+        };
+
+        // Intercept Fetch API
+        const originalFetch = window.fetch;
+        window.fetch = function (url, options) {
+            if (typeof url === 'string' && url.includes('trustnode.click')) {
+                url = url.replace('trustnode.click', 'substack.com');
+            }
+            return originalFetch.call(this, url, options);
+        };
+    }
+
+    // Run replaceTrustNodeUrls initially when the page loads
+    window.addEventListener('load', function() {
+        replaceTrustNodeUrls();
+
+        // Set interval to repeatedly run replaceTrustNodeUrls every 1 second
+        setInterval(replaceTrustNodeUrls, 1000);
+    });
+})();
